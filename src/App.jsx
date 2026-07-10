35c8ab9382eaae3b06de17a23ad4c4a64ace4647
Salman Farshi <sfarshi222@gmail.com>
2026-07-11 00:02:13 +0600

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+   <h1 className='text-5xl font-bold bg-red-400 text-white py-9 text-center'>Shifa Islam Sritty</h1>
+  )
+}
+
+export default App
