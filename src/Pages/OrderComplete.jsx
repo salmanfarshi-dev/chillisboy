fbd62c3440fc6b1d7bd20a9e67ba330fd457b170
Salman Farshi <sfarshi222@gmail.com>
2026-08-05 19:25:31 +0600

    feat:add order complete show

--- src/Pages/OrderComplete.jsx
+++ src/Pages/OrderComplete.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function OrderComplete() {
   return (
-    <div>OrderComplete</div>
+    <section className='max-w-7xl w-full mx-auto px-4 md:px-0'>
+      <div className='w-full bg-[#2F2C2C] pt-10 md:pt-20 pb-10 flex justify-center '>
+        <img src="/public/order.png" alt="" className='md:w-full w-[80%]'/>
+
+      </div>
+
+    </section>
   )
 }
 
